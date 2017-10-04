065ee2e024ef5977498ebdf88c24804b3a2814fc
rubberbandman62 <h.toedter@iks-gmbh.com>
2017-10-04 09:22:13 +0200

    update PrintingQuotes

--- src/inputProcessingAndOutput/exercise3/PrintingQuotes.java
+++ src/inputProcessingAndOutput/exercise3/PrintingQuotes.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class PrintingQuotes {
 
-	private static Scanner scanner = new Scanner(System.in);
+	private static Scanner scanner;
 
 	public static void main(String[] args) {
 		scanner = new Scanner(System.in);
